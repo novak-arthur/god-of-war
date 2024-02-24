f8bfda8df8f1ea3cb2e1326dfe0e34b1c3434183
novak-arthur <138984669+novak-arthur@users.noreply.github.com>
2024-02-23 22:10:49 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".slide-characters", {
+  slidesPerView: 3.5,
+  spaceBetween: 19,
+  freeMode: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
+    },
+    768: {
+      slidesPerView: 2.2,
+    },
+    991: {
+      slidesPerView: 3.1,
+    },
+    1200: {
+      slidesPerView: 3.8,
+    },
+  },
+});
+AOS.init({
+  duration: 1000,
+  once: true,
+})
